ecd552bcfde747a4f6b7a65bcb2931943b0da308
Emiliano Hotes <emiliano.hotes@despegar.com>
2025-08-06 11:59:19 -0300

    1.7: unicafe, paso 2

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -16,6 +16,9 @@ const App = () => {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {(good - bad) / (good + neutral + bad) || 0}</p>
+        <p>Positive: {(good / (good + neutral + bad) || 0) * 100} %</p>
       </div>
     </div>
   );
