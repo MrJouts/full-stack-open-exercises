d8c4d610e76997ec4ff13f5fd12452b8389dec3f
Emiliano Hotes <emiliano.hotes@despegar.com>
2025-08-10 18:31:58 -0300

    2.1: Información del curso paso 6

---
+++ part2/curseinfo/src/App.tsx
@@ -0,0 +1,67 @@
+const Header = (props: { course: string }) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props: { part: string; exercises: number }) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = ({
+  parts,
+}: {
+  parts: {
+    name: string;
+    exercises: number;
+  }[];
+}) => {
+  return parts.map((part) => (
+    <Part key={part.name} part={part.name} exercises={part.exercises} />
+  ));
+};
+
+const Course = ({
+  course,
+}: {
+  course: {
+    name: string;
+    parts: {
+      name: string;
+      exercises: number;
+    }[];
+  };
+}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
