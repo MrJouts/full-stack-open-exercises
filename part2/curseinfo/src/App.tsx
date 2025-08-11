09bbc3ec62718199014f8f165f78885d846df18d
Emiliano Hotes <emiliano.hotes@despegar.com>
2025-08-10 21:10:36 -0300

    2.4: Información del curso paso 9

--- part2/curseinfo/src/App.tsx
+++ part2/curseinfo/src/App.tsx
@@ -54,29 +54,54 @@ const Course = ({
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      parts: [
+        {
+          name: "Introduction to Node.js",
+          exercises: 8,
+        },
+        {
+          name: "Express basics",
+          exercises: 12,
+        },
+        {
+          name: "MongoDB integration",
+          exercises: 9,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.name} course={course} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
