64782c0537905fa25a893e1f1e94eb3300654ac4
Emiliano Hotes <emiliano.hotes@despegar.com>
2025-08-16 10:43:32 -0300

    2.9*: La Agenda Telefónica Paso 4

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const addPerson = (event: React.FormEvent) => {
     event.preventDefault();
@@ -16,14 +20,31 @@ const App = () => {
       return;
     }
 
-    setPersons([...persons, { name: newName, number: newNumber }]);
+    setPersons([
+      ...persons,
+      { name: newName, number: newNumber, id: persons.length + 1 },
+    ]);
     setNewName("");
     setNewNumber("");
   };
 
+  const filterPersons = searchTerm
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:{" "}
@@ -41,8 +62,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
+      {filterPersons.map((person) => (
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
       ))}
